a05606a50967ec50db89349282599044a2aceda7
ddomin212 <68774450+ddomin212@users.noreply.github.com>
2022-12-08 19:19:49 +0100

    commented fb.js

--- src/fb.js
+++ src/fb.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./fbc";
+import { firebaseConfig } from "./fbc"; //firebase config missinf for obvious reasons, replace with your own
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
